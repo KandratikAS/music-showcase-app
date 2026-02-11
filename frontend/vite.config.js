e3c829bffda2ce9a3879dab24c60db575f8bcb22
Archi <kandratikas@gmail.com>
2026-02-11 20:14:24 +0300

    minify

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { visualizer } from 'rollup-plugin-visualizer'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    visualizer({ open: true })
+  ],
+  build: {
+    target: 'es2015',         
+    minify: 'esbuild',         
+    chunkSizeWarningLimit: 1000, 
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+      }
+    }
+  }
 })
